92a3a8b8de41c22bfddf70b67df3984292e28dee
unknown <km123abca@gmail.com>
2020-06-21 16:33:32 +0530

    dell 21 june

--- public/javascripts/scriptfilex.js
+++ public/javascripts/scriptfilex.js
@@ -1,4 +1,6 @@
 let focussed_input="none";
+let active_row=-1;
+let totalValue=0;
 function assignFocus(elem)
 	{
 		focussed_input=elem.id;
@@ -17,7 +19,11 @@ function typed(elem)
 	
 	if(focussed_input=="qty")
 	    {    	
- 		document.querySelector("#qty").value+=elem.innerHTML;
+ 		document.querySelector("#qty").value+=elem.innerHTML; 		
+ 		document.getElementsByClassName('salesRow')[active_row].querySelector("#pqty").innerHTML=elem.innerHTML;
+ 		var pricex=document.getElementsByClassName('salesRow')[active_row].querySelector("#pprice").innerHTML;
+ 		document.getElementsByClassName('salesRow')[active_row].querySelector("#ptotpr").innerHTML=
+ 		              parseFloat(elem.innerHTML)*parseFloat(pricex);
      	}
     else
     	{
@@ -67,16 +73,44 @@ function checkWithServer(new_id)
 						  	     	document.querySelector("#desc").value     =response.Product_Desc;
 						  	     	document.querySelector("#qty").value      =1;
 						  	     	document.querySelector("#price").value    =response.Product_Price;
-						  	     	let stringToPush=`<tr>
+						  	     	let stringToPush=`<tr class="salesRow">
 						  	     						<td>${response.Product_Name}</td>
-														<td>1</td>
-														<td>${response.Product_Price}</td>
-														<td>${response.Product_Price}</td>
+														<td id="pqty">1</td>
+														<td id="pprice">${response.Product_Price}</td>
+														<td id="ptotpr">${response.Product_Price}</td>
 						  	     					  </tr>
 						  	     	 `;
 
 						  	     	document.querySelector("#billing-tbody").innerHTML=stringToPush+
-						  	     	document.querySelector("#billing-tbody").innerHTML;	
+						  	     	                         document.querySelector("#billing-tbody").innerHTML;
+
+						  	     	//update the active row and color it
+						  	     	var t_rows=document.getElementsByClassName('salesRow');
+						  	     	active_row=0; 						  	     						  	     	
+						  	     	for(var i=0;i<t_rows.length;i++)
+						  	     		t_rows[i].classList.remove("colored_row");
+						  	     	t_rows[active_row].classList.add("colored_row"); 
+
+						  	     	//update subtotal
+						  	     	totalValue+=parseFloat(response.Product_Price);
+						  	     	document.querySelector("#subtotal_pane2").innerHTML=totalValue; 
+						  	     	document.querySelector("#tax_pane2").innerHTML=(totalValue*0.05).toFixed(2);
+						  	     	document.querySelector("#total_pane2").innerHTML=
+						  	     	   (
+						  	     	   parseFloat(document.querySelector("#subtotal_pane2").innerHTML)+
+						  	     	   parseFloat(document.querySelector("#tax_pane2").innerHTML)
+						  	     	   ).toFixed(2);
+
+						  	     	//update subtotal in pane3						  	     	  
+						  	     	document.querySelector("#subtotal_pane3").innerHTML=totalValue;
+						  	     	document.querySelector("#tax_pane3").innerHTML=(totalValue*0.05).toFixed(2);
+						  	     	document.querySelector("#total_pane3").innerHTML=
+								  	     	(parseFloat(document.querySelector("#subtotal_pane3").innerHTML)+
+								  	     	 parseFloat(document.querySelector("#tax_pane3").innerHTML)
+								  	     	 ).toFixed(2);
+
+						  	     		
+						  	     	
 						  	     	document.querySelector("#scanid").value="";
 						  	     	document.querySelector("#scanid").focus();
 						  	     	}
